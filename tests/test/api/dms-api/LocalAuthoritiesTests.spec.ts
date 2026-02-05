8238791cff5c1efc0ae54502e9e0679927474951
Mikos24 <michal.swierkosz@googlemail.com>
2026-02-05 13:15:19 +0000

    Add LOCAL_AUTHORITIES_KEY to environment variables and update tests

--- tests/test/api/dms-api/LocalAuthoritiesTests.spec.ts
+++ tests/test/api/dms-api/LocalAuthoritiesTests.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Local Authorities DMS API Tests', () => {
-    const baseUrl = process.env.DMS_BASE_URL
+    const baseUrl = process.env.DMS_BASE_URL + '/mees/localauthorities';
     
     test('Valid x-functions-key returns 200 status 2', async ({ request }) => {
-        const response = await request.get(`${baseUrl}/mees/localauthorities`, {
+        const response = await request.get(`${baseUrl}`, {
             headers: {
-                'x-functions-key': 'C6QGEfzPsfVH_pkYdZYsufQCRJjb5dnRzWCFinnPC5RPAzFuQ99jsQ=='
+                'x-functions-key': process.env.LOCAL_AUTHORITIES_KEY!
             }
         });
         
