3dcf50cd031d2a0f73b99f9a3792c6a493019a42
Mikos24 <michal.swierkosz@googlemail.com>
2026-02-05 14:12:39 +0000

    Updated home page class with latest design changes

--- tests/pages/Compliance/HomePage.ts
+++ tests/pages/Compliance/HomePage.ts
@@ -5,16 +5,28 @@ import { ViewPropertiesPage } from './ViewPropertiesPage';
 
 export class HomePage extends BasePage {
     private readonly pageContext: Locator;
-    private readonly backButton: Locator;
     private readonly signOutButton: Locator;
-    private readonly viewPropertiesButton: Locator;
+    private readonly viewPropertiesLink: Locator;
+    private readonly viewGuidanceLink: Locator;
+    private readonly viewTemplatesLink: Locator;
+    private readonly viewPenaltyCalculatorLink: Locator;
+    private readonly tabPropertyRecords: Locator;
+    private readonly tabGuidance: Locator;
+    private readonly tabTemplates: Locator;
+    private readonly tabPenaltyCalculator: Locator;
 
     constructor(page: Page) {
         super(page);
         this.pageContext = page.locator('#main-content');
-        this.backButton = page.getByRole('link', { name: 'Back', exact: true });
         this.signOutButton = page.getByRole('link', { name: 'Sign out' });
-        this.viewPropertiesButton = page.getByRole('button', { name: 'View property records' });
+        this.viewPropertiesLink = page.getByRole('link', { name: 'View property records' });
+        this.viewGuidanceLink = page.getByRole('link', { name: 'View guidance' });
+        this.viewTemplatesLink = page.getByRole('link', { name: 'View templates' });
+        this.viewPenaltyCalculatorLink = page.getByRole('link', { name: 'View penalty calculator' });
+        this.tabPropertyRecords = page.getByRole('link', { name: 'Property records', exact: true })
+        this.tabGuidance = page.getByRole('link', { name: 'Guidance', exact: true });
+        this.tabTemplates = page.getByRole('link', { name: 'Templates', exact: true });
+        this.tabPenaltyCalculator = page.getByRole('link', { name: 'Penalty calculator', exact: true });
     }
 
     // Wait for the Home Page page to load
@@ -26,9 +38,15 @@ export class HomePage extends BasePage {
             {
                 pageContext: this.pageContext,
                 pageFooter: this.pageFooter,
-                viewPropertiesButton: this.viewPropertiesButton,
-                backButton: this.backButton,
-                signOutButton: this.signOutButton
+                viewPropertiesButton: this.viewPropertiesLink,
+                signOutButton: this.signOutButton,
+                viewGuidanceButton: this.viewGuidanceLink,
+                viewTemplatesButton: this.viewTemplatesLink,
+                viewPenaltyCalculatorButton: this.viewPenaltyCalculatorLink,
+                tabPropertyRecords: this.tabPropertyRecords,
+                tabGuidance: this.tabGuidance,
+                tabTemplates: this.tabTemplates,
+                tabPenaltyCalculator: this.tabPenaltyCalculator
             },
             60000);
     }
@@ -42,9 +60,11 @@ export class HomePage extends BasePage {
     }
 
     async clickViewProperties(): Promise<ViewPropertiesPage> {
-        await this.viewPropertiesButton.click();
+        await this.viewPropertiesLink.click();
         const viewPropertiesPage = new ViewPropertiesPage(this.page);
         await viewPropertiesPage.waitForPageToLoad();
         return viewPropertiesPage;
     }
+
+
 }
